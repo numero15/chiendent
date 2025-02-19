02b1b35eaa99ccf31178afd0e0391765c4f4f6fb
numero15 <nathanaeltardif@gmail.com>
2025-02-19 15:24:04 +0100

    add smear

--- InGameUI.gd
+++ InGameUI.gd
@@ -1,22 +1,19 @@
 extends Control
 
 @export var player : Player
-@export var boostGauge : TextureProgressBar
 @export var level : Node3D
 @export var graphittiLabel : Label
 var allGraphitti : int
 var countGraphitti : int
 
 func _ready():
-	player.boostChanged.connect(update)
 	allGraphitti = level.get_child(0).get_node("Billboards").get_child_count()
 	for _billboard in level.get_child(0).get_node("Billboards").get_children():
 		_billboard.billboardActivated.connect(upadteBillboardCount)
 	countGraphitti = 0
 	graphittiLabel.text = str(countGraphitti)+"/"+str(allGraphitti)
 
-func update(value, max):
-	boostGauge.value = value / max * boostGauge.max_value
+
 	
 func upadteBillboardCount():
 	countGraphitti+=1
